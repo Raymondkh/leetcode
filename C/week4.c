49f94cf9311af9ad1cd10ec23f9ef59d601453b0
Raymond <32711083+Raymondkh@users.noreply.github.com>
2021-02-25 19:20:11 +0800

    Update week4.c

--- C/week4.c
+++ C/week4.c
@@ -59,3 +59,48 @@ int main() {
     return 0;
 }
 
+/*************************************************************************
+	> File Name: 14.prime.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Thu 25 Feb 2021 06:40:27 PM CST
+ ************************************************************************/
+// 线性筛！
+#include <stdio.h>
+#define max_n 10000
+
+int prime[max_n + 5]; // 全局数组的时候不需要清空，系统自动清
+
+void init() {
+    // N = M * P
+    // 第一步枚举M
+    for (int i = 2; i <= max_n; i++) {
+        // 素数往前存
+        if (!prime[i]) prime[++prime[0]] = i;
+        // 枚举P
+        for (int j = 1; j <= prime[0]; j++) {
+            // 超出所需范围break
+            if (prime[j] * i > max_n) break;
+            // 枚举素因子
+            prime[prime[j] * i] = 1; 
+            // i即为M，能被整除时，之后的都不是最小因子了
+            if (i % prime[j] == 0) break;
+            // 因为这样不是每次都是最大因子吧？
+            // 不是的，这里枚举P是对于之后的那些合数是最小的素因子
+            // 可以自己推一遍前30，不会有重复的。
+        }
+    }
+    return ;
+
+}
+
+int main() {
+
+    init();
+    for (int i =1; i <= prime[0]; i++) {
+        printf("%d\n", prime[i]);
+    }
+
+    return 0;
+}
+
