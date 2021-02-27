6491157231ab4b6ac9352310cb5ec4e482ee5b61
Raymond <32711083+Raymondkh@users.noreply.github.com>
2021-02-27 21:55:32 +0800

    Update test.c

--- C/test.c
+++ C/test.c
@@ -60,3 +60,62 @@ int main() {
     ans = two_to_ten(arr, sizeof(arr));
     printf("%lld\n", ans);
 }
+
+/*************************************************************************
+	> File Name: kaoshi2.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Sat 27 Feb 2021 09:15:52 PM CST
+ ************************************************************************/
+
+#include <stdio.h>
+#define MAX_N 20000
+
+int arr[MAX_N + 5];
+
+void init(int *arr) {
+
+    for (int i = 2; i <= MAX_N; i++) {
+        if (!arr[i]) arr[++arr[0]] = i;
+        for (int j = 1; j <= arr[0]; j++) {
+            if (arr[j] * i > MAX_N) break;
+            arr[arr[j] * i] = 1;
+            if (i % arr[j] == 0) break;
+        }
+    }
+    return ;
+}
+
+int binary_search(int *arr, int head, int tail, int n){
+    int mid;
+    while (head <= tail) {
+        mid = (head + tail) >> 1;
+        if (arr[mid] == n) return 1;
+        if (arr[mid] < n) head = mid + 1;
+        else tail = mid - 1;
+    }
+    return 0;
+}
+
+int main() {
+    
+    int n;
+    int i = 0, j = 0, z = 0;
+    scanf("%d", &n);
+    init(arr);
+    printf("arr[0] = %d\n", arr[0]);
+    for (i = 1; i <= arr[0] + 2; i++) {
+        for (j = i; j <= arr[0] + 2; j++) {
+            z = n - arr[i] - arr[j];
+            if(binary_search(arr, j, arr[0] + 2, z)) {
+                printf("%d %d %d", arr[i], arr[j], z);
+                return 0;
+            } 
+            
+        }
+    }
+    printf("没找到吗？");
+    
+    
+    return 0;
+}
