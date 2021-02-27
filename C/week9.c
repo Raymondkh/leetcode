fc1addfc8312265596e6daa4f1699408d714007d
Raymond <32711083+Raymondkh@users.noreply.github.com>
2021-02-27 09:43:26 +0800

    Update week9.c

--- C/week9.c
+++ C/week9.c
@@ -21,3 +21,47 @@ int main() {
 }
 // 未添加宏时输出：person -> 12  此时是4个字节一个单位
 // 添加宏后输出：person -> 9   此时就是一个字节一个单位
+
+
+/*************************************************************************
+	> File Name: 23.ip.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Sat 27 Feb 2021 08:59:18 AM CST
+ ************************************************************************/
+// 将ip转化为唯一对应的整数
+#include <stdio.h>
+
+union IP {
+    struct {
+        unsigned char a1;
+        unsigned char a2;
+        unsigned char a3;
+        unsigned char a4;
+    } ip;
+    unsigned int num;
+};
+
+
+int is_little() {
+    static int num = 1;
+    return  ((char *)(&num))[0];
+}
+
+int main() {
+    // 小端机测试
+    printf("%d\n", is_little());
+    union IP p;
+    char str[100] = {0};
+    int arr[4];
+    while (~scanf("%s", str)) {
+        sscanf(str, "%d.%d.%d.%d", arr, arr + 1, arr + 2, arr + 3);
+        p.ip.a1 = arr[0];
+        p.ip.a2 = arr[1];
+        p.ip.a3 = arr[2];
+        p.ip.a4 = arr[3];
+        printf("ip = %u\n", p.num);
+    }
+
+    return 0;
+}
