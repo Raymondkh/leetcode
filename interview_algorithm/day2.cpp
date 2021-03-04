b6e424e5d717cd7638f923cda46543fc57eaeb7b
Raymond <32711083+Raymondkh@users.noreply.github.com>
2021-03-04 11:42:19 +0800

    Update day2.cpp

--- interview_algorithm/day2.cpp
+++ interview_algorithm/day2.cpp
@@ -252,3 +252,39 @@ int main() {
     return 0;
 }
 
+/*************************************************************************
+	> File Name: euler18.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Thu 04 Mar 2021 11:30:06 AM CST
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int n, num[20][20];
+
+int main() {
+    n = 15;
+    // 输入数据,注意是从1,1开始输入，边缘值为0方便计算
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cin >> num[i][j];
+        } 
+    }
+    // 逐层计算, 注意是从第二层开始
+    int ans = 0;
+    for (int i = 2; i <= n; i++) {
+        // 每层的数组都是<=层数
+        for (int j = 1; j <= n; j++) {
+	    // 左上，上的两个值的最大值
+            num[i][j] += max(num[i - 1][j], num[i - 1][j - 1]);
+            ans = max(ans, num[i][j]); // 不断记录最大值
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
+// 答案:1074
+
+
